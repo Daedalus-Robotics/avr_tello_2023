b8b2d762cd20a2509e4ba42e6ff86cf998022399
bichanna <nobu.bichanna@gmail.com>
2023-10-09 16:03:16 -0400

    Add smooth manual control for Tello

--- avr_tello_2023/screens/manual_mode.py
+++ avr_tello_2023/screens/manual_mode.py
@@ -3,19 +3,25 @@ from textual.screen import ModalScreen
 from textual.containers import Horizontal, Vertical
 from textual.binding import Binding
 from textual.widgets import Label, Button
+from textual.reactive import reactive
 from djitellopy import Tello
+
+from time import sleep
+from typing import List
 
 from constants import MANUAL_MODE_COMMANDS
 
 class ManualModeScreen(ModalScreen):
     TELLO: Tello = None
     
-    DISTANCE = 80
+    DISTANCE = 50
     ROTATION = 30
 
     BINDINGS = \
         [Binding(key=m[0], action=m[1], description=m[2]) for m in MANUAL_MODE_COMMANDS] + \
         [Binding(key='h', action='request_help', description='Show Help screen')]
+
+    vals = reactive([0, 0, 0, 0])
 
     def __init__(self, tello: Tello, app: App, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -38,6 +44,10 @@ class ManualModeScreen(ModalScreen):
         
         yield widget
 
+    def validate_vals(self, vals) -> List[int]:
+        self.TELLO.send_rc_control(*vals)
+        [0, 0, 0, 0]
+
     def on_button_pressed(self, event: Button.Pressed) -> None:
         if event.button.id == 'quitButton':
             self.dismiss()
@@ -46,31 +56,31 @@ class ManualModeScreen(ModalScreen):
         self.dismiss()
 
     def action_move_forward(self) -> None:
-        self.TELLO.move_forward(self.DISTANCE)
+        self.vals = [self.vals[0], self.DISTANCE, self.vals[2], self.vals[3]]
 
     def action_move_backward(self) -> None:
-        self.TELLO.move_back(self.DISTANCE)
-    
+        self.vals = [self.vals[0], -self.DISTANCE, self.vals[2], self.vals[3]]
+
     def action_move_left(self) -> None:
-        self.TELLO.move_left(self.DISTANCE)
-    
+        self.vals = [-self.DISTANCE, self.vals[1], self.vals[2], self.vals[3]]
+
     def action_move_right(self) -> None:
-        self.TELLO.move_right(self.DISTANCE)
+        self.vals = [self.DISTANCE, self.vals[1], self.vals[2], self.vals[3]]
 
     def action_land(self) -> None:
         self.TELLO.land()
 
     def action_rotate_cw(self) -> None:
-        self.TELLO.rotate_clockwise(self.ROTATION)
-    
+        self.vals = [self.vals[0], self.vals[1], self.vals[2], self.ROTATION]
+
     def action_rotate_ccw(self) -> None:
-        self.TELLO.rotate_counter_clockwise(self.ROTATION)
+        self.vals = [self.vals[0], self.vals[1], self.vals[2], -self.ROTATION]
 
     def action_move_up(self) -> None:
-        self.TELLO.move_up(self.DISTANCE)
-    
+        self.vals = [self.vals[0], self.vals[1], self.DISTANCE, self.vals[3]]
+
     def action_move_down(self) -> None:
-        self.TELLO.move_down(self.DISTANCE)
+        self.vals = [self.vals[0], self.vals[1], -self.DISTANCE, self.vals[3]]
 
     def action_takeoff(self) -> None:
         self.TELLO.takeoff()
