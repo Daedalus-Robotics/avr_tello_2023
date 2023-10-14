b2353de466d5cabb8fae64fb5617f5d8cf9ff0cd
bichanna <nobu.bichanna@gmail.com>
2023-10-14 16:15:24 -0400

    Add smoke_jumper module

---
+++ avr_tello_2023/smoke_jumper.py
@@ -0,0 +1,37 @@
+import serial
+
+ser = serial.Serial()
+
+
+def configure(com, baud=115200):
+    ser.baudrate = baud
+    ser.port = com
+
+
+def close_dropper() -> bool:
+    ser.open()
+
+    if ser.is_open:
+        ser.write(b"2")
+        ser.close()
+        return True
+
+    return False
+
+
+def open_dropper() -> bool:
+    ser.open()
+
+    if ser.is_open:
+        ser.write(b"1")
+        ser.close()
+
+        return True
+
+    return False
+
+
+def scan_ports(keyw="CP2104"):
+    ports = serial.tools.list_ports.comports()
+    pts = [port for port, desc, _ in sorted(ports) if keyw in desc]
+    return pts
